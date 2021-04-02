113902243671915286b93b7ccee85659f7238c00
insecurity22 <insecurity22@naver.com>
2021-04-02 18:12:50 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 import sys
 import re
 
-# 17-2 -> summary0902
+# ex. 17-2 -> summary0902
 
 path = "C:\\Users\\CT-Kang\\Desktop\\example\\"
 oName = "2"
@@ -36,17 +36,34 @@ def changeName(path, cName):
             print("폴더 이름:", filename)
 
             print("\n[변경 후]")
-            
-            # 중간에 구분 문자가 있을 때. ex) 17-2
+
+            # 1.
+            # 원본 폴더 중간에 구분 문자가 있을 때. ex) 17-2
             if oMiddleText != "":
                 # 10 이하는 앞에 0 붙이기
                 name = filename.split(".")[0].split(oMiddleText) # ['17', '9']
                 if int(name[1]) < 10:
-                    index = "0" + name[1]
-                else:
-                    index = name[1]
+                    index = "0" + str(int(name[1]))
+                else: index = str(int(name[1]))
+
+            # 원본 폴더 중간에 구분 문자가 없을 때. ex) 그림17
+            else:
+                # 10 이하는 앞에 0 붙이기
+                name = re.findall(r'[\d]*\.', filename)[0].replace(".", "")
+                if int(name) < 10: index = "0" + str(int(name)) # 0을 지우고 싶다면 이 부분 0 없애면 됨
+                else: index = str(int(name))
+
+            # 2.
+            # 변환할 폴더 중간에 구분 문자가 있을 때. ex) 그림17 -> 그림-17
+            if cMiddleText == "":
+                rename = path + cName + index + cExtension
+                print("폴더 이름:", cName + index + cExtension)
+
+            # 변환할 폴더 중간에 구분 문자가 없을 때. ex) 그림-17 -> 그림17
+            else:
+                rename = path + cName + cMiddleText + index + cExtension
                 print("폴더 이름:", cName + cMiddleText + index + cExtension)
-                print("-------------------------")
+            print("-------------------------")
 
             # 변경 확인
             if count == 0:
@@ -58,7 +75,7 @@ def changeName(path, cName):
 
                 answer = input()
                 if answer == "Y" or answer == "N":
-                    os.rename(path + filename, path + cName + cMiddleText + index + cExtension)
+                    os.rename(path + filename, rename)
                     continue
                 elif answer == "A": count = 1
                 elif answer == "E": sys.exit()
@@ -66,7 +83,7 @@ def changeName(path, cName):
             # 변경
             if count == 1: # Y
                 print("\n전체 변경되었습니다.\n")
-                os.rename(path+filename, path+cName+cMiddleText+index+cExtension)
+                os.rename(path + filename, rename)
 
         except Exception as e:
             print("--> 변경할 수 없습니다.\n")
